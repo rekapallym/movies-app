4fa747cdc673035268cca640850dda6955cf1f0e
Naga Rekapally - v0r02kq <Naga.Rekapally@walmart.com>
2022-03-22 22:09:24 -0500

    update

--- screens/Detail.js
+++ screens/Detail.js
@@ -7,6 +7,8 @@ import {
   Dimensions,
   ActivityIndicator,
   View,
+  Modal,
+  Pressable,
 } from 'react-native';
 import dateFormat from 'dateformat';
 import {getMovieDetail} from '../services/services';
@@ -14,14 +16,15 @@ import StarRating from 'react-native-star-rating';
 import PlayButton from '../components/PlayButton';
 
 const noImage = require('../assests/images/noImg.jpeg');
-
 const dimensions = Dimensions.get('screen');
-
 const Detail = ({route, navigation}) => {
   const movieId = route.params.movieId;
   const [movieDetail, setMovieDetail] = useState();
   const [loaded, setLoaded] = useState(false);
-
+  const [modalVisible, setModalVisible] = useState(false);
+  const videoShown = () => {
+    setModalVisible(!modalVisible);
+  };
   useEffect(() => {
     getMovieDetail(movieId).then(movieData => {
       setMovieDetail(movieData);
@@ -31,55 +34,66 @@ const Detail = ({route, navigation}) => {
   return (
     <React.Fragment>
       {loaded && (
-        <ScrollView>
-          <Image
-            resizeMode="cover"
-            style={styles.image}
-            source={
-              movieDetail.poster_path
-                ? {
-                    uri:
-                      'https://image.tmdb.org/t/p/w500' +
-                      movieDetail.poster_path,
-                  }
-                : noImage
-            }
-          />
-          <View style={styles.scrollContainer}>
-            <View style={styles.playButton}>
-              <PlayButton />
-            </View>
-            <Text style={styles.movieTitle}>{movieDetail.title}</Text>
-            {movieDetail.genres && (
-              <View style={styles.generesContainer}>
-                {movieDetail.genres.map(genre => {
-                  return (
-                    <Text style={styles.genre} key={genre.id}>
-                      {genre.name}
-                    </Text>
-                  );
-                })}
-              </View>
-            )}
-            <StarRating
-              disabled={true}
-              maxStars={5}
-              rating={movieDetail.vote_average}
-              fullStarColor={'gold'}
-              starSize={30}
+        <View>
+          <ScrollView>
+            <Image
+              resizeMode="cover"
+              style={styles.image}
+              source={
+                movieDetail.poster_path
+                  ? {
+                      uri:
+                        'https://image.tmdb.org/t/p/w500' +
+                        movieDetail.poster_path,
+                    }
+                  : noImage
+              }
             />
-            <Text style={styles.overview}>{movieDetail.overview}</Text>
-            <Text style={styles.releaseDate}>
-              {'Release Date: ' +
-                dateFormat(movieDetail.release_date, 'mmmm dS yyyy')}
-            </Text>
-          </View>
-        </ScrollView>
+            <View style={styles.scrollContainer}>
+              <View style={styles.playButton}>
+                <PlayButton handlePress={videoShown} />
+              </View>
+              <Text style={styles.movieTitle}>{movieDetail.title}</Text>
+              {movieDetail.genres && (
+                <View style={styles.generesContainer}>
+                  {movieDetail.genres.map(genre => {
+                    return (
+                      <Text style={styles.genre} key={genre.id}>
+                        {genre.name}
+                      </Text>
+                    );
+                  })}
+                </View>
+              )}
+              <StarRating
+                disabled={true}
+                maxStars={5}
+                rating={movieDetail.vote_average}
+                fullStarColor={'gold'}
+                starSize={30}
+              />
+              <Text style={styles.overview}>{movieDetail.overview}</Text>
+              <Text style={styles.releaseDate}>
+                {'Release Date: ' +
+                  dateFormat(movieDetail.release_date, 'mmmm dS yyyy')}
+              </Text>
+            </View>
+          </ScrollView>
+          <Modal animationType="slide" visible={modalVisible}>
+            <View style={styles.hideModel}>
+              <Pressable onPress={() => videoShown()}>
+                <Text>{'Hide model'}</Text>
+              </Pressable>
+            </View>
+          </Modal>
+        </View>
       )}
+
       {!loaded && <ActivityIndicator />}
     </React.Fragment>
   );
 };
+
 const styles = StyleSheet.create({
   image: {height: dimensions.height / 2},
   scrollContainer: {justifyContent: 'center', alignItems: 'center', flex: 1},
@@ -108,6 +122,11 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: -29,
     right: 20,
+  },
+  hideModel: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
   },
 });
 
